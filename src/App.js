15caa48bc0002b824576d2152470a56a908bd34a
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-07 06:44:44 -0500

    App/Index
    - Wrapped Header and Dashboard in a Router
    - Fixed app import error (importing from wrong location)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Dashboard from "./layout/common/Dashboard";
 import Header from "./layout/common/Header";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <Dashboard />
+      <Container>
+        <Router>
+          <Header />
+          <Dashboard />
+        </Router>
+      </Container>
     </div>
   );
 }
