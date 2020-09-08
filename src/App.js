175e1d5b66a3efe36770f28acb63683d2fea2b8b
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-08 07:44:37 -0500

    Css edits
    - Removed unused css from App.css
    - Imported it into App.js
    - Added css for container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import Dashboard from "./layout/common/Dashboard";
 import Header from "./layout/common/Header";
-import Container from "react-bootstrap/Container";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
   return (
     <div>
-      <Container>
-        <Router>
-          <Header />
-          <Dashboard />
-        </Router>
-      </Container>
+      <Router>
+        <Header />
+        <Dashboard />
+      </Router>
     </div>
   );
 }
