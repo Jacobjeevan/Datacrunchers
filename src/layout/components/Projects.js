6c62f1e649a07e9be48124a8b5a98116338ffdd7
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-08 07:48:21 -0500

    Projects fetch/css
    - Changed Projects to function based to use React query (hooks)
    - Imported newly created api (to call backend)
    - React query to handle data fetching results
    - Displaying results in a container
    - Created a css file for styling (css folder creation as well)

--- src/layout/components/Projects.js
+++ src/layout/components/Projects.js
@@ -1,7 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
+import { useQuery } from "react-query";
+import fetchProjects from "../../api/projects";
+import "../../css/projects.css";
+import Container from "react-bootstrap/Container";
 
-export default class Projects extends Component {
-  render() {
-    return <div></div>;
+export default function Projects() {
+  const { isLoading, isError, data, error } = useQuery(
+    "projectData",
+    fetchProjects
+  );
+
+  if (isLoading) {
+    return <span>Loading...</span>;
   }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <Container>
+      <div className="project-container">
+        {data.map((project) => (
+          <div key={project.id} className="project">
+            <div className="title">{project.title}</div>
+            <div className="description">{project.description}</div>
+            <div className="authors">{project.authors}</div>
+            <div className="github">{project.github}</div>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 }
