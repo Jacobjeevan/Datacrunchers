333bd59449aeb9191478d1eb00ad2147f7aa8c18
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-09 07:48:56 -0500

    Projectform Updates
    - Converted Projectform to function based
    	- This allows using React query for post requests (once form is submitted)
    	- TO DO: React-query for post updates

--- src/layout/components/private/Projectsform.js
+++ src/layout/components/private/Projectsform.js
@@ -1,56 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 import FormField from "./Formfield";
 import "../../../css/projectsform.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-export default class Projectsform extends Component {
-  handleClick = () => {
-    this.props.toggle();
-  };
+export default function Projectsform(props) {
+  return (
+    <div className="formContent">
+      <span className="closeBtn" onClick={props.toggle}></span>
+      <Form>
+        <FormField
+          control="ProjectName"
+          label="Name"
+          type="text"
+          placeholder="Enter Name"
+          text="Enter the name of the Project"
+        />
 
-  render() {
-    return (
-      <div className="formContent">
-        <span className="closeBtn" onClick={this.handleClick}></span>
-        <Form>
-          <FormField
-            control="ProjectName"
-            label="Name"
-            type="text"
-            placeholder="Enter Name"
-            text="Enter the name of the Project"
-          />
+        <FormField
+          control="ProjectDesc"
+          label="Description"
+          type="text"
+          placeholder="About the Project"
+          text="Enter brief information about the Project"
+        />
 
-          <FormField
-            control="ProjectDesc"
-            label="Description"
-            type="text"
-            placeholder="About the Project"
-            text="Enter brief information about the Project"
-          />
+        <FormField
+          control="ProjectAuthor"
+          label="Authors"
+          type="text"
+          placeholder="Authors"
+          text="Enter Name of Authors"
+        />
 
-          <FormField
-            control="ProjectAuthor"
-            label="Authors"
-            type="text"
-            placeholder="Authors"
-            text="Enter Name of Authors"
-          />
+        <FormField
+          control="ProjectLink"
+          label="Github"
+          type="text"
+          placeholder="Github Link"
+          text="Enter Link to the Project"
+        />
 
-          <FormField
-            control="ProjectLink"
-            label="Github"
-            type="text"
-            placeholder="Github Link"
-            text="Enter Link to the Project"
-          />
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
-    );
-  }
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
