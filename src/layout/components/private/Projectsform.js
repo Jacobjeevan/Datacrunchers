07e45ff9622ae80ca40db2d9d401839b95d8ab3f
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-10 08:21:41 -0500

    Project Form
    - Handled OnChange and Submit events
            - formInput-hook (useState for state management)
            - React Query for invalidating queries (for refetching) once
    form is submitted
    - Removed placeholder values for FormFields (too much clutter)
    
    To Do: Integrate with Auth0 (pass in authorization token for logged
    in users, as well as hide Projectform for unauthenticated users).

--- src/layout/components/private/Projectsform.js
+++ src/layout/components/private/Projectsform.js
@@ -3,42 +3,75 @@ import FormField from "./Formfield";
 import "../../../css/projectsform.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useMutation, queryCache } from "react-query";
+import { addProject } from "../../../api/projects";
+/* import { useAuth0 } from "@auth0/auth0-react";
+ */
+import { useInput } from "./formInput-hook";
 
 export default function Projectsform(props) {
+  // const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [mutate] = useMutation(
+    (body) => {
+      return addProject(body);
+    },
+    {
+      onSuccess: () => {
+        queryCache.invalidateQueries("projectData");
+      },
+    }
+  );
+  const { value: title, bind: bindTitle, reset: resetTitle } = useInput("");
+  const { value: description, bind: bindDesc, reset: resetDesc } = useInput("");
+  const { value: authors, bind: bindAuthors, reset: resetAuthors } = useInput(
+    ""
+  );
+  const { value: github, bind: bindGithub, reset: resetGithub } = useInput("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const body = { title, description, authors, github };
+    mutate(body);
+    resetTitle();
+    resetDesc();
+    resetAuthors();
+    resetGithub();
+  };
+
   return (
     <div className="formContent">
       <span className="closeBtn" onClick={props.toggle}></span>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormField
-          control="ProjectName"
-          label="Name"
+          control="ProjectTitle"
+          label="Title"
           type="text"
-          placeholder="Enter Name"
           text="Enter the name of the Project"
+          {...bindTitle}
         />
 
         <FormField
           control="ProjectDesc"
           label="Description"
           type="text"
-          placeholder="About the Project"
           text="Enter brief information about the Project"
+          {...bindDesc}
         />
 
         <FormField
           control="ProjectAuthor"
           label="Authors"
           type="text"
-          placeholder="Authors"
           text="Enter Name of Authors"
+          {...bindAuthors}
         />
 
         <FormField
           control="ProjectLink"
           label="Github"
           type="text"
-          placeholder="Github Link"
           text="Enter Link to the Project"
+          {...bindGithub}
         />
 
         <Button variant="primary" type="submit">
