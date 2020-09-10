0594afc6b7d136a9ba480f0e0d433597b88554ee
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-10 08:18:16 -0500

    FormField additions
    - Added Onchange and value props to Form Control under FormField Component

--- src/layout/components/private/Formfield.js
+++ src/layout/components/private/Formfield.js
@@ -5,7 +5,12 @@ export default function Formfield(props) {
   return (
     <Form.Group controlId={props.control}>
       <Form.Label>{props.label}</Form.Label>
-      <Form.Control type={props.type} placeholder={props.placeholder} />
+      <Form.Control
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        value={props.value}
+      />
       <Form.Text className="text-muted">{props.text}</Form.Text>
     </Form.Group>
   );
