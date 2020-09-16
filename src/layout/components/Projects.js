aca828ecb3ee524b9d41297fcef60c321a26355e
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-16 08:21:08 -0500

    CSS styles
    - Added additional styling for Projects

--- src/layout/components/Projects.js
+++ src/layout/components/Projects.js
@@ -7,7 +7,6 @@ import {
   updateProject,
 } from "../../api/projects";
 import "../../css/projects.css";
-import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { mutate } from "swr";
 
@@ -26,7 +25,6 @@ const defaultFormDisplay = {
 export default function Project() {
   const [displayForm, setdisplayForm] = useState(defaultFormDisplay);
   const [formValues, setformValues] = useState(defaultFormValues);
-
   const { isLoading, data, error } = useGetProjects();
 
   if (isLoading) {
@@ -75,40 +73,44 @@ export default function Project() {
 
   return (
     <div>
-      <Button variant="primary" onClick={toggleCreateForm}>
+      <button onClick={toggleCreateForm} className="submitBtn">
         Add Project
-      </Button>{" "}
+      </button>
+
       {displayForm.createForm ? (
         <Projectsform toggle={toggleCreateForm} onSubmit={handleAdd} />
       ) : null}
       {displayForm.editForm ? (
         <Projectsform
-          toggle={toggleEditForm}
+          toggle={toggleEditForm.bind(null, false)}
           onSubmit={handleEdit}
           initialFormValues={formValues}
         />
       ) : null}
+
       <div className="project-container">
         {data.map((project) => (
           <div key={project._id} className="project">
-            <div className="title">{project.title}</div>
-            <div className="description">{project.description}</div>
-            <div className="authors">{project.authors}</div>
-            <div className="github">{project.github}</div>
-            <Button
-              variant="danger"
-              type="button"
-              onClick={handleDelete.bind(this, project._id)}
-            >
-              Delete
-            </Button>
-            <Button
-              variant="info"
-              type="button"
-              onClick={handleEditButton.bind(this, project)}
-            >
-              Edit
-            </Button>
+            <div className="project-meta">
+              <div className="title">{project.title}</div>
+              <div className="description">{project.description}</div>
+              <div className="authors">{project.authors}</div>
+              <div className="github">{project.github}</div>
+            </div>
+            <div className="cardBtn-container">
+              <button
+                className="editBtn cardBtn"
+                onClick={handleEditButton.bind(this, project)}
+              >
+                Edit
+              </button>
+              <button
+                onClick={handleDelete.bind(this, project._id)}
+                className="deleteBtn cardBtn"
+              >
+                Delete
+              </button>
+            </div>
           </div>
         ))}
       </div>
