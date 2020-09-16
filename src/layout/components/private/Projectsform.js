54b84366886bdff2529342ff5345452032c23f1a
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-16 08:21:45 -0500

    CSS Styles/Transitions
    - Additional css for projectforms
    - Installed Spring library
    - Added transitions with spring library for projectforms

--- src/layout/components/private/Projectsform.js
+++ src/layout/components/private/Projectsform.js
@@ -2,10 +2,10 @@ import React from "react";
 import FormField from "./Formfield";
 import "../../../css/projectsform.css";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 /* import { useAuth0 } from "@auth0/auth0-react";
  */
 import { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
 
 const defaultFormValues = {
   title: "",
@@ -20,6 +20,10 @@ export default function Projectsform({
   initialFormValues = defaultFormValues,
 }) {
   const [formValues, setformValues] = useState(initialFormValues);
+  const [props, set] = useSpring(() => ({
+    opacity: 1,
+    from: { opacity: 0 },
+  }));
 
   const setformValue = (field, value) =>
     setformValues((old) => ({ ...old, [field]: value }));
@@ -30,56 +34,75 @@ export default function Projectsform({
 
   // const { isAuthenticated, getAccessTokenSilently } = useAuth0();
 
+  function changeEffect() {
+    set({
+      opacity: 0,
+      from: {
+        opacity: 1,
+      },
+    });
+  }
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
     onSubmit(formValues);
     setformValues(defaultFormValues);
   };
 
+  const handleCancel = () => {
+    changeEffect();
+    setTimeout(() => toggle(), 200);
+  };
+
   return (
-    <div className="formContent">
-      <span className="closeBtn" onClick={toggle}></span>
-      <Form onSubmit={handleSubmit}>
-        <FormField
-          control="ProjectTitle"
-          label="Title"
-          type="text"
-          text="Enter the name of the Project"
-          value={formValues.title}
-          onChange={(e) => setformValue("title", e.target.value)}
-        />
+    <animated.div style={props}>
+      <div className="formContent">
+        <Form onSubmit={handleSubmit}>
+          <FormField
+            control="ProjectTitle"
+            label="Title"
+            type="text"
+            text="Enter the name of the Project"
+            value={formValues.title}
+            onChange={(e) => setformValue("title", e.target.value)}
+          />
 
-        <FormField
-          control="ProjectDesc"
-          label="Description"
-          type="text"
-          text="Enter brief information about the Project"
-          value={formValues.description}
-          onChange={(e) => setformValue("description", e.target.value)}
-        />
+          <FormField
+            control="ProjectDesc"
+            label="Description"
+            type="text"
+            text="Enter brief information about the Project"
+            value={formValues.description}
+            onChange={(e) => setformValue("description", e.target.value)}
+          />
 
-        <FormField
-          control="ProjectAuthor"
-          label="Authors"
-          type="text"
-          text="Enter Name of Authors"
-          value={formValues.authors}
-          onChange={(e) => setformValue("authors", e.target.value)}
-        />
+          <FormField
+            control="ProjectAuthor"
+            label="Authors"
+            type="text"
+            text="Enter Name of Authors"
+            value={formValues.authors}
+            onChange={(e) => setformValue("authors", e.target.value)}
+          />
 
-        <FormField
-          control="ProjectLink"
-          label="Github"
-          type="text"
-          text="Enter Link to the Project"
-          value={formValues.github}
-          onChange={(e) => setformValue("github", e.target.value)}
-        />
+          <FormField
+            control="ProjectLink"
+            label="Github"
+            type="text"
+            text="Enter Link to the Project"
+            value={formValues.github}
+            onChange={(e) => setformValue("github", e.target.value)}
+          />
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
+          <button type="submit" className="submitBtn">
+            Submit
+          </button>
+
+          <button type="button" className="cancelBtn" onClick={handleCancel}>
+            Cancel
+          </button>
+        </Form>
+      </div>
+    </animated.div>
   );
 }
