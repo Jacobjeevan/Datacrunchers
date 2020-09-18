7be224879d99c4f7301037c701a874c530d9b878
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-18 06:37:17 -0500

    Auth0
    - Slight changes as per Auth0's react usage recommendations
    	- Better integration with react-router (maintaining user navigation history within auth0 -> proper redirection after login)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Dashboard from "./layout/common/Dashboard";
 import Header from "./layout/common/Header";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
   return (
     <div>
-      <Router>
-        <Header />
-        <Dashboard />
-      </Router>
+      <Header />
+      <Dashboard />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth0provider-history";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={process.env.REACT_APP_domain}
-    clientId={process.env.REACT_APP_clientId}
-    redirectUri={window.location.origin}
-    audience={process.env.REACT_APP_audience}
-    scope="read:current_user update:current_user_metadata"
-  >
-    <App />
-  </Auth0Provider>,
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
   document.getElementById("root")
 );
