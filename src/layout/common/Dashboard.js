95d6c88446c51c4e72276d584002a84fec9a3b7c
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-22 11:00:35 -0500

    Dashboard components
    - Added components (Events, Careers)  to Dashboard
    - Additional styling for Dashboard

--- src/layout/common/Dashboard.js
+++ src/layout/common/Dashboard.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import Events from "../components/Events";
 import Officers from "../components/Officers";
+import Events from "../components/Events";
 import Projects from "../components/Projects";
 import Resources from "../components/Resources";
-import Career from "../components/Career";
+import Careers from "../components/Careers";
 import { Switch, Route } from "react-router-dom";
 import "../../css/dashboard.css";
 import Container from "react-bootstrap/Container";
@@ -13,11 +13,13 @@ export default class Dashboard extends Component {
     return (
       <Container className="dashboard">
         <Switch>
-          <Route exact path="/" component={Officers} />
-          <Route exact path="/events" component={Events} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/resources" component={Resources} />
-          <Route exact path="/career-prep" component={Career} />
+          <div className="dashboard-body">
+            <Route exact path="/" component={Officers} />
+            <Route exact path="/events" component={Events} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/resources" component={Resources} />
+            <Route exact path="/career-prep" component={Careers} />
+          </div>
         </Switch>
       </Container>
     );
