b45e8e9351fe3ebb1a910da2b490b07c7e4319e7
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-10-13 10:19:12 -0500

    rm console.log for toast

--- src/Components/Main/Notifications.js
+++ src/Components/Main/Notifications.js
@@ -2,8 +2,6 @@ import { toast } from "react-toastify";
 
 export const ToastAlerts = (data) => {
   let { success, message } = data;
-  console.log(data);
-  console.log(success);
   if (success) {
     toast.success(message);
   } else {
